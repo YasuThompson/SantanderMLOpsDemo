1f6872a30ac5594fedaf7256c607fad5d35c1edf
YasutoDATANOMIQ <yasuto.tamura@datanomiq.de>
2024-03-24 22:28:19 +0100

    Managed to make the script run and train the model

--- santander_dir/baseline_book.py
+++ santander_dir/baseline_book.py
@@ -16,22 +16,14 @@ prods = ['ind_ahor_fin_ult1', 'ind_aval_fin_ult1', 'ind_cco_fin_ult1', 'ind_cder
              'ind_ecue_fin_ult1', 'ind_fond_fin_ult1', 'ind_hip_fin_ult1', 'ind_plan_fin_ult1', 'ind_pres_fin_ult1', 'ind_reca_fin_ult1',
              'ind_tjcr_fin_ult1', 'ind_valo_fin_ult1', 'ind_viv_fin_ult1', 'ind_nomina_ult1', 'ind_nom_pens_ult1', 'ind_recibo_ult1']
 
-categorical_cols = ['ind_empleado', 'pais_residencia', 'sexo', 'tiprel_1mes', 'indresi', 'indext', 'conyuemp', 'canal_entrada', 'indfall', 'tipodom', 'nomprov', 'segmento']
 
-train_path = '../data/santander/train_ver2.csv'
-test_path = '../data/santander/test_ver2.csv'
+train_path = '../data/santander-product-recommendation/train_ver2_small.csv'
+test_path = '../data/santander-product-recommendation/test_ver2.csv'
 
-# use_dates = ['2015-01-28', '2015-02-28', '2015-03-28', '2015-04-28',
-#        '2015-05-28', '2015-06-28', '2015-07-28', '2015-08-28',
-#        '2015-09-28', '2015-10-28', '2015-11-28', '2015-12-28',
-#        '2016-01-28', '2016-02-28', '2016-03-28', '2016-04-28',
-#        '2016-05-28']
-
-# test_date =  '2016-06-28'
-
-
-use_dates = ['2016-02-28', '2016-03-28', '2016-04-28', '2016-05-28']
-test_date =  '2016-06-28'
+# use_dates = ['2016-02-28', '2016-03-28', '2016-04-28', '2016-05-28']
+use_dates = ['2015-01-28', '2015-02-28', '2015-03-28']
+val_dates = ['2015-03-28']
+test_dates =  ['2016-06-28']
 
 param = {
         'booster': 'gbtree',
@@ -57,21 +49,21 @@ def main():
     # DADA LAODING. 
     # Preferably in an SQL server or a distributed storage. 
     print("Loading data from csv files")
-    trn = pd.read_csv(train_path, low_memory=False)
-    tst = pd.read_csv(test_path, low_memory=False)
+    trn = pd.read_csv(train_path, sep=',', low_memory=False)
+    tst = pd.read_csv(test_path, sep=',',  low_memory=False)
     
     #### Phase 2 ####
     # DATA CLEANING. 
     # Preferably completed before SQL server. 
-    print("Cleaning and joining training data.")
+    print("Cleaninng data.")
     df = clean_data(trn, tst)
     
     #### Phase 3 ####
     # Data engineering. 
     # Preferably implemented with SQL or Apache Spark.
     # Preferably saved to a feature store. 
-    print("Cleaning and joining training data.")
-    trn, tst, features = data_engineering(df, use_dates, test_date)
+    print("Labeling data.")
+    trn, tst, features = data_engineering(df, use_dates, test_dates)
     
     
     """
@@ -82,13 +74,13 @@ def main():
     
     ↓ DATA SCIENCE
     """
-    
+
     
     
     #### Phase 4 ####
     # DATA SPLIT FOR CROSS VALIDATION. 
     print("Splitting into training and validation data as Numpy arrays..")
-    X_trn, Y_trn, X_vld, Y_vld = split_data(trn, features)
+    X_trn, Y_trn, X_vld, Y_vld = split_data(trn, features, val_dates)
     
     
     #### Phase 5 ####
@@ -97,10 +89,10 @@ def main():
     dvld = xgb.DMatrix(X_vld, label=Y_vld, feature_names=features)
     watch_list = [(dtrn, 'train'), (dvld, 'eval')]
     print("Training a model")
-    model = xgb.train(param, dtrn, num_boost_round=10, evals=watch_list, early_stopping_rounds=20)
+    model = xgb.train(param, dtrn, num_boost_round=10, evals=watch_list, early_stopping_rounds=1)
     
     pickle.dump(model, open("./model/xgb.baseline.pkl", "wb"))
-    best_ntree_limit = model.best_ntree_limit
+    # best_ntree_limit = model.best_ntree_limit
 
     #dall = xgb.DMatrix(X_all, label=Y_all, feature_names=features)
     #best_ntree_limit = int(best_ntree_limit * (len(XY_trn) + len(XY_vld)) / len(XY_trn))
@@ -115,7 +107,8 @@ def main():
     print("Inference on test data")    
     X_tst = tst[features].values
     dtst = xgb.DMatrix(X_tst, feature_names=features)
-    preds_tst = model.predict(dtst, ntree_limit=best_ntree_limit)
+    # preds_tst = model.predict(dtst, ntree_limit=best_ntree_limit)
+    preds_tst = model.predict(dtst)
     ncodpers_tst = tst['ncodpers'].values
     #preds_tst = preds_tst - tst.as_matrix(columns=[prod + '_prev' for prod in prods])
     preds_tst = preds_tst - tst[[prod + '_prev' for prod in prods]].values
