30ee92da16e43d78d7826a1b1d57af63df7e53cf
YasutoDATANOMIQ <yasuto.tamura@datanomiq.de>
2024-03-25 08:46:13 +0100

    Got rid of warnings, and adjusted data split part

--- santander_dir/process_data_santander.py
+++ santander_dir/process_data_santander.py
@@ -15,7 +15,7 @@ categorical_cols = ['ind_empleado', 'pais_residencia', 'sexo', 'tiprel_1mes', 'i
 def clean_data(trn, tst):
     
     # 製品の変数を別途に保存しておきます。
-    prods = trn.columns[24:].tolist()
+    # prods = trn.columns[24:].tolist()
 
     # 製品変数の欠損値をあらかじめ0に代替しておきます。
     trn[prods] = trn[prods].fillna(0.0).astype(np.int8)
@@ -33,24 +33,27 @@ def clean_data(trn, tst):
     #features = []
 
     # カテゴリ変数を .factorize() 関数に通して label encodingします。
-#     categorical_cols = ['ind_empleado', 'pais_residencia', 'sexo', 'tiprel_1mes', 'indresi', 'indext', 'conyuemp', 'canal_entrada', 'indfall', 'tipodom', 'nomprov', 'segmento']
+
     for col in categorical_cols:
-        df[col], _ = df[col].factorize(na_sentinel=-99)
+        df[col], _ = df[col].factorize()
+
+    # df = pd.get_dummies(df, columns=categorical_cols)
+
     #features += categorical_cols
 
     # 数値型変数の特異値と欠損値を -99に代替し、整数型に変換します。
-    df['age'].replace(' NA', -99, inplace=True)
+    df['age'] = df['age'].replace(' NA', -99)
     df['age'] = df['age'].astype(np.int8)
 
-    df['antiguedad'].replace('     NA', -99, inplace=True)
+    df['antiguedad'] = df['antiguedad'].replace('     NA', -99)
     df['antiguedad'] = df['antiguedad'].astype(np.int8)
 
-    df['renta'].replace('         NA', -99, inplace=True)
-    df['renta'].fillna(-99, inplace=True)
+    df['renta'] = df['renta'].replace('         NA', -99)
+    df['renta'] = df['renta'].fillna(-99)
     df['renta'] = df['renta'].astype(float).astype(np.int8)
 
-    df['indrel_1mes'].replace('P', 5, inplace=True)
-    df['indrel_1mes'].fillna(-99, inplace=True)
+    df['indrel_1mes'] = df['indrel_1mes'].replace('P', 5)
+    df['indrel_1mes'] = df['indrel_1mes'].fillna(-99)
     df['indrel_1mes'] = df['indrel_1mes'].astype(float).astype(np.int8)
 
     # 学習に使用する数値型変数を featuresに追加します。
@@ -61,7 +64,7 @@ def clean_data(trn, tst):
     return df
 
 
-def data_engineering(df, use_dates, test_date):
+def data_engineering(df, use_dates, test_dates):
     
     features = []
     
@@ -78,7 +81,7 @@ def data_engineering(df, use_dates, test_date):
     features += ['ult_fec_cli_1t_month', 'ult_fec_cli_1t_year']
 
     # それ以外の変数の欠損値をすべて -99に代替します。
-    df.fillna(-99, inplace=True)
+    df = df.fillna(-99)
     
     
     
@@ -105,8 +108,8 @@ def data_engineering(df, use_dates, test_date):
     # 前の月の製品情報が存在しない場合に備えて、0に代替します。
     for prod in prods:
         prev = prod + '_prev'
-        df_trn[prev].fillna(0, inplace=True)
-    df_trn.fillna(-99, inplace=True)
+        df_trn[prev] = df_trn[prev].fillna(0)
+    df_trn = df_trn.fillna(-99)
 
     # lag-1 変数を追加します。
     features += [feature + '_prev' for feature in features]
@@ -120,17 +123,16 @@ def data_engineering(df, use_dates, test_date):
     ## モデル学習
     # 学習のため、データを訓練、検証用に分離します。
     # 学習には 2016-01-28 ~ 2016-04-28 のデータだけを使用し、検証には 2016-05-28 のデータを使用します。
-    #use_dates = ['2016-01-28', '2016-02-28', '2016-03-28', '2016-04-28', '2016-05-28']
-#use_dates = ['2016-03-28', '2016-04-28', '2016-05-28']
+
     trn = df_trn[df_trn['fecha_dato'].isin(use_dates)]
-    tst = df_trn[df_trn['fecha_dato'] == '2016-06-28']
+    tst = df_trn[df_trn['fecha_dato'].isin(test_dates)]
     del df_trn
     
     
     return trn, tst, features
 
 
-def split_data(trn, features):
+def split_data(trn, features, val_dates):
     
     # 訓練データから新規購買件数だけを抽出します。
     X = []
@@ -146,13 +148,11 @@ def split_data(trn, features):
     XY['y'] = Y
 
     # 訓練、検証データに分離します。
-    #vld_date = '2016-05-28'
-    vld_dates = ['2016-05-28']
-    
+
     #XY_trn = XY[XY['fecha_dato'] != vld_date]
     #XY_vld = XY[XY['fecha_dato'] == vld_date]
-    XY_trn = XY[~XY['fecha_dato'].isin(vld_dates)]
-    XY_vld = XY[XY['fecha_dato'].isin(vld_dates)]
+    XY_trn = XY[~XY['fecha_dato'].isin(val_dates)]
+    XY_vld = XY[XY['fecha_dato'].isin(val_dates)]
 
     # 訓練、検証データを XGBoost 形態に変換します。
     X_trn = XY_trn[features].values
@@ -165,11 +165,7 @@ def split_data(trn, features):
     
     return X_trn, Y_trn, X_vld, Y_vld
 
-    
-# def get_xgb_data(X_trn, Y_trn, X_vld, Y_vld, features):
-#     dtrn = xgb.DMatrix(X_trn, label=Y_trn, feature_names=features)
-#     dvld = xgb.DMatrix(X_vld, label=Y_vld, feature_names=features)
-#     return dtrn, dvld
+
 
     
     
