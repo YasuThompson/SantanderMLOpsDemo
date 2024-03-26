e5264142a945461f24eeedc23c8f55da8b01ea30
YasutoDATANOMIQ <yasuto.tamura@datanomiq.de>
2024-03-26 19:16:45 +0100

    Finished script to clean data locally, with Pandas

--- data_prep/clean_data.py
+++ data_prep/clean_data.py
@@ -1,67 +1,72 @@
+import yaml
 import numpy as np
 import pandas as pd
+
+def take_value_dict(key, dict):
+    try:
+        return dict[key]
+    except:
+        return key
 
 def load_local_data(csv_path):
     return pd.read_csv(csv_path, sep=',', low_memory=False)
 
-def clean_data(trn, tst, prod_cols, categorical_cols):
-    # 製品変数の欠損値をあらかじめ0に代替しておきます。
-    trn[prod_cols] = trn[prod_cols].fillna(0.0).astype(np.int8)
 
-    # 24個の製品を1つも保有していない顧客のデータを除去します。
-    no_product = trn[prod_cols].sum(axis=1) == 0
-    trn = trn[~no_product]
-
-    # 訓練データとテストデータを統合します。テストデータにない製品変数は0で埋めます。
-    for col in trn.columns[24:]:
-        tst[col] = 0
-    df = pd.concat([trn, tst], axis=0)
-
-    # カテゴリ変数を .factorize() 関数に通して label encodingします。
-    for col in categorical_cols:
-        df[col], _ = df[col].factorize()
+def read_lists_from_yaml(file_path):
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+        return data
 
 
-    # 数値型変数の特異値と欠損値を -99に代替し、整数型に変換します。
-    df['age'] = df['age'].replace(' NA', -99)
-    df['age'] = df['age'].astype(np.int8)
+def clean_data_pandas(df):
 
-    df['antiguedad'] = df['antiguedad'].replace('     NA', -99)
-    df['antiguedad'] = df['antiguedad'].astype(np.int8)
+    # Getting some column names and their mappings from a yaml file
+    config_dict = read_lists_from_yaml('data_prep/data_prep_config.yaml')
 
-    df['renta'] = df['renta'].replace('         NA', -99)
-    df['renta'] = df['renta'].fillna(-99)
-    df['renta'] = df['renta'].astype(float).astype(np.int8)
+    column_map_dict = config_dict['col_name_map_dict'] # A dictionary mapping column names
+    target_cols = config_dict['target_cols'] # Target columns to predict
 
-    df['indrel_1mes'] = df['indrel_1mes'].replace('P', 5)
-    df['indrel_1mes'] = df['indrel_1mes'].fillna(-99)
-    df['indrel_1mes'] = df['indrel_1mes'].astype(float).astype(np.int8)
+    # Renaming column names
+    df = df.rename(columns=column_map_dict)
 
-    # 学習に使用する数値型変数を featuresに追加します。
-    # features += ['age','antiguedad','renta','ind_nuevo','indrel','indrel_1mes','ind_actividad_cliente']
+    # Renaming columns
+    target_cols = [take_value_dict(col, column_map_dict) for col in target_cols]
+
+    # When product information is nan, replacing them with 0
+    df[target_cols] = df[target_cols].fillna(0.0).astype(np.int8)
+
+    # Deliting customers who have no products (could harm prediction)
+    no_product = df[target_cols].sum(axis=1) == 0
+    df = df[~no_product]
+
+
+    # Filling nan values of numeric data
+    df[column_map_dict['age']] = df[column_map_dict['age']].replace(' NA', -99)
+    df[column_map_dict['age']] = df[column_map_dict['age']].astype(np.int8)
+
+    df[column_map_dict['antiguedad']] = df[column_map_dict['antiguedad']].replace('     NA', -99)
+    df[column_map_dict['antiguedad']] = df[column_map_dict['antiguedad']].astype(np.int8)
+
+    df[column_map_dict['renta']] = df[column_map_dict['renta']].replace('         NA', -99)
+    df[column_map_dict['renta']] = df[column_map_dict['renta']].fillna(-99)
+    df[column_map_dict['renta']] = df[column_map_dict['renta']].astype(float).astype(np.int8)
+
+    df[column_map_dict['indrel_1mes']] = df[column_map_dict['indrel_1mes']].replace('P', 5)
+    df[column_map_dict['indrel_1mes']] = df[column_map_dict['indrel_1mes']].fillna(-99)
+    df[column_map_dict['indrel_1mes']] = df[column_map_dict['indrel_1mes']].astype(float).astype(np.int8)
 
     return df
 
 
 if __name__ == '__main__':
-
     data_source = 'csv'
-    train_path = '../data/santander-product-recommendation/train_ver2_small.csv'
-    test_path = '../data/santander-product-recommendation/test_ver2.csv'
+    train_path = 'data/santander-product-recommendation/train_ver2_small.csv'
 
-    save_path = 'data_cleaned.csv'
+    save_path = 'data_prep/train_cleaned_small.csv'
 
     if data_source=='csv':
         print("Loading data from csv files")
-
-        trn = load_local_data(train_path)
-        tst = load_local_data(test_path)
-
-    #### Phase 2 ####
-    # DATA CLEANING.
-    # Preferably completed before SQL server.
-    print("Cleaninng data.")
-    df_cleaned = clean_data(trn, tst)
-
-
-    df_cleaned.to_csv(save_path, index=False)
+        df_train = load_local_data(train_path)
+        print("Cleaninng data.")
+        df_cleaned = clean_data_pandas(df_train)
+        df_cleaned.to_csv(save_path, index=False)
