8a87ebc26718b5337e6a754aa24afbc30c73785d
YasutoDATANOMIQ <yasuto.tamura@datanomiq.de>
2024-03-30 09:52:49 +0100

    Generalized data cleaning more

--- data_prep/clean_data.py
+++ data_prep/clean_data.py
@@ -17,56 +17,87 @@ def read_lists_from_yaml(file_path):
         data = yaml.safe_load(file)
         return data
 
+def clean_num_element(x, replace_val=-99):
+    """
+    Cleans an element in an object column of a pandas dataframe
+    :param x:
+    :param replace_val:
+    :return:
+    """
 
-def clean_data_pandas(df, data_config_path='data_prep_config.yaml'):
+    if pd.isna(x) == True:
+        replaced_value = replace_val
+
+    else:
+        try:
+            replaced_value = int(x)
+        except:
+            try:
+                replaced_value = float(x)
+            except:
+                replaced_value = replace_val
+
+
+
+    # print('(x, replaced_value): {}'.format((x, replaced_value)))
+
+    return replaced_value
+
+
+
+
+
+def clean_data_pandas(df, config_dict):
 
     # Getting some column names and their mappings from a yaml file
-    config_dict = read_lists_from_yaml(data_config_path)
-
     column_map_dict = config_dict['col_name_map_dict'] # A dictionary mapping column names
-    target_cols = config_dict['target_cols'] # Target columns to predict
 
     # Renaming column names
     df = df.rename(columns=column_map_dict)
 
     # Renaming columns
-    target_cols = [take_value_dict(col, column_map_dict) for col in target_cols]
+    config_dict_cleaned = {}
+    config_dict_cleaned['numeric_columns'] = [take_value_dict(col, column_map_dict) for col in config_dict['numeric_columns']]
+    config_dict_cleaned['categorical_columns'] = [take_value_dict(col, column_map_dict) for col in config_dict['categorical_columns']]
+    config_dict_cleaned['date_columns'] = [take_value_dict(col, column_map_dict) for col in config_dict['date_columns']]
+    config_dict_cleaned['product_columns'] = [take_value_dict(col, column_map_dict) for col in config_dict['product_columns']]
 
     # When product information is nan, replacing them with 0
-    df[target_cols] = df[target_cols].fillna(0.0).astype(np.int8)
+    df[config_dict_cleaned['product_columns']] = df[config_dict_cleaned['product_columns']].fillna(0.0).astype(np.int8)
 
-    # Deliting customers who have no products (could harm prediction)
-    no_product = df[target_cols].sum(axis=1) == 0
+    # Deleting customers who have no products (could harm prediction)
+    no_product = df[config_dict_cleaned['product_columns']].sum(axis=1) == 0
     df = df[~no_product]
 
-
     # Filling nan values of numeric data
-    df[column_map_dict['age']] = df[column_map_dict['age']].replace(' NA', -99)
-    df[column_map_dict['age']] = df[column_map_dict['age']].astype(np.int8)
+    for num_col in config_dict_cleaned['numeric_columns']:
+        df[num_col] = df[num_col].apply(lambda x: clean_num_element(x))
 
-    df[column_map_dict['antiguedad']] = df[column_map_dict['antiguedad']].replace('     NA', -99)
-    df[column_map_dict['antiguedad']] = df[column_map_dict['antiguedad']].astype(np.int8)
+    # (Feature engineering) label encoding
+    # Convert date columns to datetime objects
+    for date_col in config_dict_cleaned['date_columns']:
+        df[date_col] = pd.to_datetime(df[date_col], errors='coerce')
 
-    df[column_map_dict['renta']] = df[column_map_dict['renta']].replace('         NA', -99)
-    df[column_map_dict['renta']] = df[column_map_dict['renta']].fillna(-99)
-    df[column_map_dict['renta']] = df[column_map_dict['renta']].astype(float).astype(np.int8)
-
-    df[column_map_dict['indrel_1mes']] = df[column_map_dict['indrel_1mes']].replace('P', 5)
-    df[column_map_dict['indrel_1mes']] = df[column_map_dict['indrel_1mes']].fillna(-99)
-    df[column_map_dict['indrel_1mes']] = df[column_map_dict['indrel_1mes']].astype(float).astype(np.int8)
-
-    return df
+    return df, config_dict_cleaned
 
 
 if __name__ == '__main__':
     data_source = 'csv'
     train_path = 'data/santander-product-recommendation/train_ver2_small.csv'
+    data_config_path = 'data_prep/data_prep_config.yaml'
 
-    save_path = 'data_prep/train_cleaned_small.csv'
+    data_save_path = 'data_prep/train_cleaned_small.csv'
+    config_save_path = 'data_prep/data_prep_config_cleaned.yaml'
+
+    config_dict = read_lists_from_yaml(data_config_path)
 
     if data_source=='csv':
         print("Loading data from csv files")
         df_train = load_local_data(train_path)
         print("Cleaninng data.")
-        df_cleaned = clean_data_pandas(df_train)
-        df_cleaned.to_csv(save_path, index=False)
+        df_cleaned, config_dict_cleaned = clean_data_pandas(df_train, config_dict)
+
+        df_cleaned.to_csv(data_save_path, index=False)
+
+        with open(config_save_path, 'w') as file:
+            yaml.dump(config_dict_cleaned, file)
